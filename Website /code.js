cc18da8699e3ac18c999de1e6d2ada5c1afc3909
navypenguin <hyangchild@gmail.com>
2023-12-02 11:17:12 -0800

    hi

--- Website /code.js
+++ Website /code.js
@@ -7,3 +7,22 @@ function showListValue() {
     alert("Entered item: " + listVal);
     document.getElementById("output").innerHTML = listVal;
   }
+  function addTextToList() {
+    // Get the input value
+    var inputValue = document.getElementById("textInput").value;
+
+    // If the input is not empty, add it to the list
+    if (inputValue.trim() !== "") {
+        // Create a new list item
+        var listItem = document.createElement("li");
+
+        // Set the text content of the list item
+        listItem.textContent = inputValue;
+
+        // Append the list item to the ul element
+        document.getElementById("textList").appendChild(listItem);
+
+        // Clear the input field
+        document.getElementById("textInput").value = "";
+    }
+}
