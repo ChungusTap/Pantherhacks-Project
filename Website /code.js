fede7516a21eacf21f21bd202ad29300e24b2ee1
navypenguin <hyangchild@gmail.com>
2023-12-02 13:23:42 -0800

    h

--- Website /code.js
+++ Website /code.js
@@ -1,7 +1,14 @@
+<<<<<<< Updated upstream
 function clearTextList() {
     // Clear all the list items
     var textList = document.getElementById("textList");
     textList.innerHTML = "";
+=======
+function myfunction(){
+    var newItem = document.getElementById("textList").children[index];
+    newItem.classList.toggle("completed");
+    
+>>>>>>> Stashed changes
 }
 
 function editFunction() {
@@ -24,7 +31,15 @@ function showListValue() {
     var listVal = document.getElementById("listValue").value;
     alert("Entered item: " + listVal);
     document.getElementById("output").innerHTML = listVal;
+<<<<<<< Updated upstream
 }
+=======
+  }
+function toggleItem(index) {
+    var listVar = document.getElementById("textList").children[index];
+    listVar.classList.toggle("completed");
+  }
+>>>>>>> Stashed changes
 
 function addTextToList() {
     // Get the input value
@@ -33,15 +48,29 @@ function addTextToList() {
     // If the input is not empty, add it to the list
     if (inputValue.trim() !== "") {
         // Create a new list item
+        var checklist = document.getElementById("textList");
         var listItem = document.createElement("li");
 
         // Set the text content of the list item
         listItem.textContent = inputValue;
 
+        listItem.addEventListener("click", function() {
+            toggleItem(Array.from(checklist.children).indexOf(listItem));
+          });
+
         // Append the list item to the ul element
+<<<<<<< Updated upstream
         document.getElementById("textList").appendChild(listItem);
         
         // Clear the input field
         document.getElementById("textInput").value = "";
     }
+=======
+        checklist.appendChild(listItem);
+        // Clear the input field
+        document.getElementById("textInput").value = "";
+        
+    }
+    
+>>>>>>> Stashed changes
 }
